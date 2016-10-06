e7f7628f1c8863ad32eebf74efe94ec5b9d5e8f4
Gavin Zhou <gavin.zhou@gmail.com>
2016-10-07 00:03:38 +0900

    fix tag mistake

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ for path in $dockerfiles; do
   tag=$(echo $path | sed 's@/@:@' | sed 's@/@-@g')
   log_msg "Tagging docker image $tag with gcr tag"
   docker tag "orangesys/alpine-orangesys-influxdb:${tag}" "asia.gcr.io/orange-sys/alpine-orangesys-influxdb:${tag}"
-  sudo /opt/google-cloud-sdk/bin/gcloud docker push asia.gcr.io/orange-sys/alpine-orangesys-orangesys-influxdb:${tag}
+  sudo /opt/google-cloud-sdk/bin/gcloud docker push asia.gcr.io/orange-sys/alpine-orangesys-influxdb:${tag}
 
 done
