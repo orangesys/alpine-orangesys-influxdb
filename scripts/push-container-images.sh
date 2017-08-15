95550e60e84c9ae9d90d2482e730d7448b56b4b3
gavinzhou <gavin.zhou@gmail.com>
2017-08-15 16:05:44 +0900

    test ci 1

--- scripts/push-container-images.sh
+++ scripts/push-container-images.sh
@@ -3,6 +3,8 @@ set -ex
 
 version=$(git describe --always --tags|sed 's/^v//')
 
+docker push orangesys/${CIRCLE_PROJECT_REPONAME}
+
 echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/account-auth.json
 gcloud auth activate-service-account --key-file ${HOME}/account-auth.json
 gcloud config set project $PROJECT_NAME
