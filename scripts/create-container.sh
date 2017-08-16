5bc99ad4ccf25982d2f5a714a2b962e247d766b5
gavinzhou <gavin.zhou@gmail.com>
2017-08-16 13:15:24 +0900

    remove ls directory

--- scripts/create-container.sh
+++ scripts/create-container.sh
@@ -7,4 +7,3 @@ docker build --tag "orangesys/${CIRCLE_PROJECT_REPONAME}:${version}" .
 docker images
 mkdir -p /caches
 docker save -o /caches/${CIRCLE_PROJECT_REPONAME}.tar "orangesys/${CIRCLE_PROJECT_REPONAME}:${version}"
-ls /caches/
