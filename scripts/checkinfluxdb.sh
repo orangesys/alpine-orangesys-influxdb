fc2d0af6243aee2a33f45ca48458d77727e2e236
gavinzhou <gavin.zhou@gmail.com>
2017-08-16 13:20:53 +0900

    test ci 3

---
+++ scripts/checkinfluxdb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+
+version=$(git describe --always --tags|sed 's/^v//')
+docker run -d --name influx -p 8086:8086 orangesys/alpine-orangesys-influxdb:${version}
+
+docker run --network container:influx \
+		appropriate/curl -I 'http://127.0.0.1:8086/ping'
+
+docker run --network container:influx \
+		appropriate/curl -sI 'http://127.0.0.1:8086/ping' | grep -q "X-Influxdb-Version"
