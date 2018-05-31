552310146d648094d6d4797c2a688e913891a1fc
gavinzhou <gavin.zhou@gmail.com>
2018-05-31 18:13:16 +0900

    changed docker curl

--- .circleci/checkinfluxdb.sh
+++ .circleci/checkinfluxdb.sh
@@ -5,7 +5,7 @@ version=$(git describe --always --tags)
 docker run -d --name influx -p 8086:8086 orangesys/alpine-orangesys-influxdb:${version}
 
 docker run --network container:influx \
-		appropriate/curl -I 'http://127.0.0.1:8086/ping'
+		orangesys/docker-curl -I 'http://127.0.0.1:8086/ping'
 
 docker run --network container:influx \
-		appropriate/curl -sI 'http://127.0.0.1:8086/ping' | grep -q "X-Influxdb-Version"
+		orangesys/docker-curl -sI 'http://127.0.0.1:8086/ping' | grep -q "X-Influxdb-Version"
